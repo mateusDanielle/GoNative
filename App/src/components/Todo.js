03a416f99829cdd64dc20b1d4309d91d061858f0
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-14 15:59:11 -0200

    PropTypes

--- App/src/components/Todo.js
+++ App/src/components/Todo.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default class Todo extends Component {
   static defaultProps = {
     title: "Todo padrão"
+  };
+
+  static propTypes = {
+    title: PropTypes.string
   };
   render() {
     return (
