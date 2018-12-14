7b3af7ac568abb430e7fd8c69fb8879c68e3be5d
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-14 15:56:27 -0200

    DefaultProps

--- App/src/components/Todo.js
+++ App/src/components/Todo.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
 export default class Todo extends Component {
+  static defaultProps = {
+    title: "Todo padrão"
+  };
   render() {
     return (
       <View>
