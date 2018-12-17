52ff3a32cbf331591215ea553a6f6e15f265f304
Mateus Danielle <mateusdanielle@edu.fecap.br>
2018-12-17 11:48:03 -0200

    Rectotron

--- App/src/index.js
+++ App/src/index.js
@@ -1,7 +1,11 @@
+import "./config/ReactotronConfig";
+
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, Button } from "react-native";
 
 import Todo from "./components/Todo";
+
+console.tron.log("Teste");
 
 export default class App extends Component {
   state = {
